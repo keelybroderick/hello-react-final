e6a42f26f1f00377c88d9687439b4dc2a117fa81
Keely Broderick <broder35@msu.edu>
2023-03-31 22:09:21 -0400

    zines

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
 
+const magazines = {
+  { id: 1, title: 'Architectural Digest', theme: 'architecture', isAvailable: true },
+  { id: 2, title: 'Dwell', theme: 'architecture', isAvailable: true },
+  { id: 3, title: 'Communication Arts', theme: 'design', isAvailable: false },
+};
+
+function ZineRack () {
+  const listZines = magazines.map(zine ==>
+    <li
+      key={zine.ed}
+      style={{
+        color: zine.isAvailable ? 'green' : 'red'
+      }}
+    >
+      {zine.title}
+    </li>
+  );
+  return (
+    <ul>{listZines}</ul>
+  )
+}
+
+const book = {
+  title: 'A Farwell to Arms',
+  author: 'Earnest Hemingway',
+  published: '1929',
+  image: '',
+  width: '264',
+  height: '378'
+}
+
+function Bookshelf() {
+  return (
+    <>
+      <h2></h2>
+      <p></p>
+      <img>
+        className="bookCover"
+        src={book.title + 'cover'}
+        style={{
+          width: book.width,
+          height: book.height
+        }}
+      </img>
+    </>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with MSUandme !
-        </a>
+        <ZineRack />
+        <Bookshelf />
+        <MagicButton />
       </header>
     </div>
   );
