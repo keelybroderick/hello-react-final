fd24c7a5fdc7e13503c64cf8d703b60effbf0922
Keely Broderick <broder35@msu.edu>
2023-04-03 19:01:20 -0400

    fixederrors

--- src/App.js
+++ src/App.js
@@ -2,6 +2,33 @@ import { useState } from 'react';
 import { supabase } from './supabaseClient';
 import './App.css';
 
+function Library() {
+  const [myBook, setMyBooks] = useState([]);
+  async function getBooks() {
+    let { data: books, error } = await supabase
+      .from('books')
+      .select('*')
+    setMyBooks(books);
+  }
+  getBooks();
+  return (
+    <table>
+    {
+      setMyBooks.map(b => {
+        <tr>
+          <td>{b.title}</td>
+          <td>{b.author}</td>
+          <td>{b.isbn}</td>
+        </tr>
+      })
+    }
+    </table>
+  );
+}
+
+
+
+
 const magazines = [
 { id: 1, title: 'Architectural Digest', theme: 'architecture', isAvailable: true },
 { id: 2, title: 'Dwell', theme: 'architecture', isAvailable: true },
@@ -73,9 +100,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <ZineRack />
-      <Bookshelf />
-      <MagicButton />
+        <Library />
+        <ZineRack />
+        <Bookshelf />
+        <MagicButton />
       </header>
     </div>
   );
